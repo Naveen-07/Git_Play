1e02407920e402439c964baef2caaa5a5f4cbd3f
Naveen-07 <naveennani0711@gmail.com>
2021-08-02 09:51:52 +0530

    added gitignore

--- abc.java
+++ abc.java
@@ -1,6 +1,7 @@
-class Demo {
+class Demo{
 	public static void main(String[] args){
 		System.out.println("Welcome to Git Play");
-		System.out.println("Added a feature");
+		System.out.println("Added a feature Sample");
+
 	}
 }
